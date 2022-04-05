c8fef418bba6cdcd273f82236d7e23690b528e6e
yaukhutadzianis <102165346+yaukhutadzianis@users.noreply.github.com>
2022-04-05 20:05:49 +0300

    useReducer in process

--- src/components/hooks/useReducer/UseReducerHook.js
+++ src/components/hooks/useReducer/UseReducerHook.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 export default function UseReducerHook() {
+  const initialState = { count: 0 };
+
+  function countReducer(state, action) {
+    switch (action) {
+      case "incrementAction":
+        return { count: state.count + 1 };
+      case "decrementAction":
+        return { count: state.count - 1 };
+
+      default:
+        break;
+    }
+  }
+
+  const [state, dispatch] = useReducer(countReducer, initialState);
+
   return (
     <div className="container-fluid py-5 bg-secondary">
-      <div className="container py-3">1</div>
+      <div className="container py-3">
+        <h1 className="fs-3">
+          This counter use useReducer hook without useState
+        </h1>
+        <p>{state.count}</p>
+        <button
+          className="btn btn-warning me-3"
+          onClick={() => dispatch("incrementAction")}
+        >
+          Increment
+        </button>
+        <button
+          className="btn btn-warning me-3"
+          onClick={() => dispatch("decrementAction")}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
